83eefd237974de19b80b0ff8cb74025fb85d424e
Joey <2419466090@qq.com>
2023-03-01 23:20:32 +0800

    cahnges now are up to date

--- miniprogram/pages/publish/publish.js
+++ miniprogram/pages/publish/publish.js
@@ -5,6 +5,45 @@ Page({
    * 页面的初始数据
    */
   data: {
+    List: [{
+        imageurl: '/images/快递.png',
+        descri: '取/寄快递',
+      },
+      {
+        imageurl: '/images/社区食堂-01.png',
+        descri: '食堂/外卖代取',
+      },
+      {
+        imageurl: '/images/设备租赁.png',
+        descri: '租借设备',
+      },
+      {
+        imageurl: '/images/合作.png',
+        descri: '帮忙做',
+      }
+    ]
+  },
+  
+
+  onPublish(e){
+  var id=e.currentTarget.dataset.id;
+  if (!id){
+    wx.navigateTo({
+      url: '/pages/publish-table/publish-table?id='+id,
+    })
+  }else if (id==1){
+    wx.navigateTo({
+      url: '/pages/publish-table-canteen/publish-table-canteen?id='+id,
+    })
+  }else if (id==2){
+    wx.navigateTo({
+      url: '/pages/publish-table-borrow/publish-table-borrow?id='+id,
+    })
+  }else{
+    wx.navigateTo({
+      url: '/pages/publish-table-help/publish-table-help?id='+id,
+    })
+  }
 
   },
 
